e5b02688d11741af054a001e8f578cefc87b5d6a
kokethebest <koke.rojas@gmail.com>
2012-08-16 01:11:27 -0400

    terminamos el sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def destroy
+    	sign_out
+    	redirect_to root_url		
+	end
+
+	def create
+
+  		user = User.find_by_email(params[:session][:email].downcase)
+  
+  		if user && user.authenticate(params[:session][:password])
+      		sign_in user
+      		redirect_to user
+  		else
+    		# Create an error message and re-render the signin form.
+    		flash.now[:error] = "La combinacin Usuario/Password es incorrecta"
+    		render 'new'
+  		end
+
+
+	end
+
+
+end
